d9838c72aba10193dfd1761a56298707ba1f330d
mahmadamir <ansariahmad342@gmail.com>
2023-09-16 23:34:13 +0500

    init

---
+++ src/app/dashboard/ahmad/page.js
@@ -0,0 +1,13 @@
+// sections
+
+import OneView from 'src/sections/one/view';
+
+// ----------------------------------------------------------------------
+
+export const metadata = {
+  title: 'Dashboard: Ahmad',
+};
+
+export default function Page() {
+  return <OneView />;
+}
---
+++ src/app/dashboard/husnain/page.js
@@ -0,0 +1,41 @@
+// sections
+
+import OneView from 'src/sections/one/view';
+
+// ----------------------------------------------------------------------
+
+export const metadata = {
+  title: 'Dashboard: husnain',
+};
+
+export default function Page() {
+  return (
+    <>
+      <p>
+        <span>Hello</span>
+      </p>
+      <p>
+        <span>jbskxabsksax</span>
+      </p>
+      <p>&nbsp;</p>
+      <p>
+        <span>asc</span>
+      </p>
+      <p>
+        <span>acsa</span>
+      </p>
+      <p>
+        <span>cas</span>
+      </p>
+      <p>
+        <span>cSa</span>
+      </p>
+      <p>
+        <span>cSaCsaCsa</span>
+      </p>
+      <p>
+        <span>csac</span>
+      </p>
+    </>
+  );
+}
